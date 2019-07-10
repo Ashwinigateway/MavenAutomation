8fe63692ea18d4e11f9b93611796b5d000dc99a3
Ashwinigateway <37830540+Ashwinigateway@users.noreply.github.com>
2019-07-10 18:50:26 +0530

    Update TestBase.java

--- src/test/java/com/gateway/automationpractice/tests/TestBase.java
+++ src/test/java/com/gateway/automationpractice/tests/TestBase.java
@@ -2,6 +2,7 @@ package com.gateway.automationpractice.tests;
 
 import com.gateway.automationpractice.utils.Log;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
